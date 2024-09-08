b1e47ab9ccb10cd2d11d7bb90267dd0ad6033755
Vikash0132 <159477917+Vikash0132@users.noreply.github.com>
2024-09-08 16:16:31 +0530

    navbar saptak la lauda dala

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
 export default function Navbar() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    // Handle the search functionality here.
+    // You may need to pass data or filter results as needed.
+    console.log('Search Query:', searchQuery);
+  };
+
   return (
-    <div className="heading">
-      <h1>Touristo</h1>
-    </div>
+    <nav>
+      <div className="nav-container">
+        <div className="heading">
+          <h1>Touristo</h1>
+        </div>
+        <form onSubmit={handleSearchSubmit}>
+          <input 
+            type="text" 
+            placeholder="Search..." 
+            value={searchQuery} 
+            onChange={handleSearch} 
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
   );
 }
