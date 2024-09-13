eb303d5f6be7d4088303197d31704751ff88ec7f
Vikash Choudhary <vc0410251@gmail.com>
2024-09-13 12:39:52 +0530

    gpt not search and map

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Navbar from './components/navbar.jsx';
-import './App.css';
 import Map from './components/map.jsx';
+import './App.css';
+
 function App() {
+  const location = useLocation(); // Get the current route
+
+  return (
+    <div className="App">
+      {/* Conditionally render Navbar and Map based on the current route */}
+      {location.pathname !== '/' && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      {location.pathname !== '/' && <Map />} {/* Show Map only on non-login pages */}
+    </div>
+  );
+}
+
+// Wrapping the app in Router for proper navigation
+export default function AppWrapper() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-        <Map />
-      </div>
+      <App />
     </Router>
   );
 }
-export default App;
