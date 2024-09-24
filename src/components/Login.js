641091f6f5a19bf95a8508207a93bac34dd52a13
Vikash Choudhary <vc0410251@gmail.com>
2024-09-25 01:46:08 +0530

    bug fix: login page style fix and no login till refresh

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './login.css';
+
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
     // Redirect to dashboard if already authenticated
     if (localStorage.getItem('authToken')) {
-      navigate('/dashboard');
+      navigate('/dashboard', { replace: true }); // Ensure replace to prevent going back to login page
     }
   }, [navigate]);
 
@@ -25,36 +28,18 @@ const Login = () => {
         localStorage.setItem('authToken', response.data.token);
         navigate('/dashboard', { replace: true });  // Redirect to dashboard after successful login
       } else {
-        alert('Invalid credentials');
+        setError('Invalid credentials');
       }
     } catch (error) {
-      console.error('There was an error logging in!', error);
+      setError('There was an error logging in!');
+      console.error('Login error:', error);
     }
   };
 
-  const containerStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    backgroundImage: 'url("/map2.jpeg")',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  };
-
-  const formStyle = {
-    padding: '20px',
-    borderRadius: '10px',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    backgroundColor: 'rgba(255,255,255,0.2)',
-    width: '100%',
-    maxWidth: '400px',
-  };
-
   return (
-    <div style={containerStyle}>
-      <form onSubmit={handleSubmit} style={formStyle}>
-        <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Login</h2>
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
         <input
           type="text"
           value={username}
@@ -68,6 +53,7 @@ const Login = () => {
           placeholder="Password"
         />
         <button type="submit">Login</button>
+        {error && <p className="error-message">{error}</p>}
       </form>
     </div>
   );
