3e98a5c414b3b1f2768be36aeb778fae90d76937
Vikash Choudhary <vc0410251@gmail.com>
2025-03-27 00:37:47 +0530

    revert back to without any location detection

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use(cors({
-  origin: 'https://touristguideproject.vercel.app'  // Allow only your frontend domain
-}));
+app.use(
+  cors({
+    origin: "https://touristguideproject.vercel.app", // Allow only your frontend domain
+  })
+);
 app.use(bodyParser.json());
 
 const users = [
-  { username: 'Vikash', password: 'Vikash12345' },
-  { username: 'Saptak', password: 'Saptak12345' },
-  { username: 'Kapish', password: 'Kapish12345' },
-  { username: 'Yuvan', password: 'Yuvan12345' }
+  { username: "Vikash", password: "Vikash12345" },
+  { username: "Saptak", password: "Saptak12345" },
+  { username: "Kapish", password: "Kapish12345" },
+  { username: "Yuvan", password: "Yuvan12345" },
 ];
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
-  const user = users.find((u) => u.username === username && u.password === password);
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
   if (user) {
     res.json({ success: true });
   } else {
@@ -26,3 +30,4 @@ app.post('/login', (req, res) => {
 });
 
 module.exports = app;
+//hey
